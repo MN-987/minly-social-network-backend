527f352927324dbde473a95c813a64e041b96a7d
Mostafa Nasser <mostafanasserm98@gmail.com>
2024-04-06 06:39:54 +0200

    intial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+
+import express from 'express'
+import bootstrap from './routes/index.router'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 6000
+
+bootstrap(app, express)
+app.listen(port, () => console.log(` app listening on port ${port}!`)) 
